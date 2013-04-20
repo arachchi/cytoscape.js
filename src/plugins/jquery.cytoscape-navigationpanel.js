1c61e281cb7fdb46b70d7fc1b94424338a7f6bb1
bumbu <bmbalex@gmail.com>
2013-04-21 01:54:06 +0300

    change indentation from spaces to tabs

--- src/plugins/jquery.cytoscape-navigationpanel.js
+++ src/plugins/jquery.cytoscape-navigationpanel.js
@@ -1,32 +1,32 @@
 ;(function($){
-  
-  var defaults = {};
-  
-  $.fn.cytoscapeNavigationpanel = function(params){
-    var options = $.extend(true, {}, defaults, params);
-    var fn = params;
-    
-    var functions = {
-      destroy: function(){
-        
-      },
-        
-      init: function(){
-        
-      }
-    };
-    
-    if( functions[fn] ){
-      return functions[fn].apply(this, Array.prototype.slice.call( arguments, 1 ));
-    } else if( typeof fn == 'object' || !fn ) {
-      return functions.init.apply( this, arguments );
-    } else {
-      $.error("No such function `"+ fn +"` for jquery.cyNavigationpanel");
-    }
-    
-    return $(this);
-  };
+	
+	var defaults = {};
+	
+	$.fn.cytoscapeNavigationpanel = function(params){
+		var options = $.extend(true, {}, defaults, params);
+		var fn = params;
+		
+		var functions = {
+			destroy: function(){
+				
+			},
+				
+			init: function(){
+				
+			}
+		};
+		
+		if( functions[fn] ){
+			return functions[fn].apply(this, Array.prototype.slice.call( arguments, 1 ));
+		} else if( typeof fn == 'object' || !fn ) {
+			return functions.init.apply( this, arguments );
+		} else {
+			$.error("No such function `"+ fn +"` for jquery.cyNavigationpanel");
+		}
+		
+		return $(this);
+	};
 
-  $.fn.cyNavigationpanel = $.fn.cytoscapeNavigationpanel;
-  
+	$.fn.cyNavigationpanel = $.fn.cytoscapeNavigationpanel;
+	
 })(jQuery);
