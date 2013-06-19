7e9b316cc357acf609d920b5e1b1140774fd8c77
bumbu <bmbalex@gmail.com>
2013-06-19 23:49:01 +0300

    remove semicolons again. take care of jshint

--- src/plugins/jquery.cytoscape-navigator.js
+++ src/plugins/jquery.cytoscape-navigator.js
@@ -3,8 +3,8 @@
 	"use strict";
 
 	var Navigator = function ( element, options ) {
-		this.init(element, options);
-	};
+		this.init(element, options)
+	}
 
 	Navigator.prototype = {
 
@@ -12,85 +12,85 @@
 
 	, init: function ( element, options ) {
 			this.$element = $(element)
-			this.options = $.extend(true, {}, $.fn.cytoscapeNavigator.defaults, options);
+			this.options = $.extend(true, {}, $.fn.cytoscapeNavigator.defaults, options)
 			this.initPanel()
 		}
 
 	, destroy: function () {
-			this.$element.remove();
+			this.$element.remove()
 		}
 
 	, initPanel: function () {
-			var options = this.options;
+			var options = this.options
 
 			if( options.container ) {
 				if( options.container instanceof jQuery ){
 					if( options.container.length > 0 ){
-						this.$panel = options.container.first();
+						this.$panel = options.container.first()
 
 						// Add class name
-						options.forceClassName && this.$panel.addClass(options.className);
+						options.forceClassName && this.$panel.addClass(options.className)
 					} else {
-						$.error("Container for jquery.cyNavigator is empty");
-						return;
+						$.error("Container for jquery.cyNavigator is empty")
+						return
 					}
 				} else if ( $(options.container).length > 0 ) {
-					this.$panel = $(options.container).first();
+					this.$panel = $(options.container).first()
 
 					// Add class name
-					options.forceClassName && this.$panel.addClass(options.className);
+					options.forceClassName && this.$panel.addClass(options.className)
 				} else {
-					$.error("There is no any element matching your selector for jquery.cyNavigator");
-					return;
+					$.error("There is no any element matching your selector for jquery.cyNavigator")
+					return
 				}
 			} else {
-				this.$panel = $('<div class="'+options.className+'"/>');
-				this.$element.append(this.$panel);
+				this.$panel = $('<div class="'+options.className+'"/>')
+				this.$element.append(this.$panel)
 			}
 
 			// TODO accept all described options
 			// TODO move to other function
-			this.$panel.width(options.size.width);
-			this.$panel.height(options.size.height);
-			this.$panel.css({top: options.position.vertical, left: options.position.horizontal});
+			this.$panel.width(options.size.width)
+			this.$panel.height(options.size.height)
+			this.$panel.css({top: options.position.vertical, left: options.position.horizontal})
 
-			this.initThumbnail();
+			this.initThumbnail()
 		}
 
 	, initThumbnail: function () {
-			this.$thumbnail = $('<dib class="cytoscape-navigatorThumbnail"/>');
+			this.$thumbnail = $('<dib class="cytoscape-navigatorThumbnail"/>')
 
 			var navigatorRatio = 1.0 * this.$panel.width() / this.$panel.height()
 				, navigatorThumbnailRatio = 1.0 * this.$element.width() / this.$element.height()
-				;
+				
 
 			if( navigatorRatio > navigatorThumbnailRatio ) {
 				// panel width is bigger than thumbnail width
-				this.$thumbnail.width(navigatorThumbnailRatio * this.$panel.height());
-				this.$thumbnail.height(this.$panel.height());
-				this.$thumbnail.css({left: (this.$panel.width() - this.$thumbnail.height())/2});
+				this.$thumbnail.width(navigatorThumbnailRatio * this.$panel.height())
+				this.$thumbnail.height(this.$panel.height())
+				this.$thumbnail.css({left: (this.$panel.width() - this.$thumbnail.height())/2})
 			} else {
 				// panel height is bigger than thumbnail height
-				this.$thumbnail.width(this.$panel.width());
-				this.$thumbnail.height(navigatorThumbnailRatio * this.$panel.width());
-				this.$thumbnail.css({top: (this.$panel.height() - this.$thumbnail.width())/2});
+				this.$thumbnail.width(this.$panel.width())
+				this.$thumbnail.height(navigatorThumbnailRatio * this.$panel.width())
+				this.$thumbnail.css({top: (this.$panel.height() - this.$thumbnail.width())/2})
 			}
 
 			// TODO Populate thumbnail with a render of graph
 
 			// Add thumbnail to the dom
-			this.$panel.append(this.$thumbnail);
+			this.$panel.append(this.$thumbnail)
 
-			this.initView();
+			this.initView()
 		}
 
 	, initView: function () {
 			var that = this
 				// , cy = this.$element.cytoscape('get')
-				;
+				
 
-			this.$view = $('<div class="cytoscape-navigatorView"/>');
-			this.$thumbnail.append(this.$view);
+			this.$view = $('<div class="cytoscape-navigatorView"/>')
+			this.$thumbnail.append(this.$view)
 
 			// Make navigator view draggable
 			// TODO get rid of jQuery UI 
@@ -101,27 +101,27 @@
 			, drag: function () {
 					if( that.options.live ) {
 						// TODO move only when cy fineshed previous rendering
-						that.moveCy();
+						that.moveCy()
 					}
 				}
 			, stop: function () {
 					if( !that.options.live ) {
-						that.moveCy();
+						that.moveCy()
 					}
 				}
-			});
+			})
 
 			// TODO find a way to stop propadation of mousemove. May be achived by replacing jQuery UI
 			this.$view.on('click.navigator mousedown.navigator touchstart.navigator ', function (ev) {
-				ev.stopPropagation();
+				ev.stopPropagation()
 			})
 
 			// Set default navigaion view size
-			this.setView();
+			this.setView()
 
 			// Hook cy zoom
 			this.$element.cytoscape('get').on('zoom pan', function () {
-				that.setView();
+				that.setView()
 			})
 			// TODO hook cy move/pan
 		}
@@ -143,40 +143,42 @@
 				, elementHeight = this.$element.height()
 				, cyWidth = elementWidth * cyZoom
 				, cyHeight = elementHeight * cyZoom
-				;
+				
 
 			if( cyPan.x > elementWidth || cyPan.x < -cyWidth || cyPan.y > elementHeight || cyPan.y < -cyHeight) {
-				visible = false;
-				this.$view.hide();
+				visible = false
+				this.$view.hide()
 			} else {
-				visible = true;
+				visible = true
 
 				// Horizontal computation
-				position.left = -thumbnailWidth * (cyPan.x / cyWidth);
-				position.right = position.left + (thumbnailWidth / cyZoom);
+				position.left = -thumbnailWidth * (cyPan.x / cyWidth)
+				position.right = position.left + (thumbnailWidth / cyZoom)
 
 				// Limit view inside thumbnails borders
-				position.left = Math.max(0, position.left);
-				position.right = Math.min(thumbnailWidth, position.right);
+				position.left = Math.max(0, position.left)
+				position.right = Math.min(thumbnailWidth, position.right)
 
 				// Compute width and remove position.right
-				width = position.right - position.left;
-				delete position.right;
+				width = position.right - position.left
+				;// for delete
+				delete position.right
 
 				// Vertical computation
-				position.top = -thumbnailHeight * (cyPan.y / cyHeight);
-				position.bottom = position.top + (thumbnailHeight / cyZoom);
+				position.top = -thumbnailHeight * (cyPan.y / cyHeight)
+				position.bottom = position.top + (thumbnailHeight / cyZoom)
 
 				// Limit view inside thumbnails borders
-				position.top = Math.max(0, position.top);
-				position.bottom = Math.min(thumbnailHeight, position.bottom);
+				position.top = Math.max(0, position.top)
+				position.bottom = Math.min(thumbnailHeight, position.bottom)
 
 				// Compute width and remove position.right
-				height = position.bottom - position.top;
-				delete position.bottom;
+				height = position.bottom - position.top
+				;// for delete
+				delete position.bottom
 
 				// Set computed values
-				this.$view.show().width(width).height(height).css(position);
+				this.$view.show().width(width).height(height).css(position)
 			}
 
 		}
@@ -199,12 +201,12 @@
 				, elementHeight = this.$element.height()
 				, cyWidth = elementWidth * cyZoom
 				, cyHeight = elementHeight * cyZoom
-				;
+				
 
-			cyPanNew.x = -position.left * cyWidth / thumbnailWidth;
-			cyPanNew.y = -position.top * cyHeight / thumbnailHeight;
+			cyPanNew.x = -position.left * cyWidth / thumbnailWidth
+			cyPanNew.y = -position.top * cyHeight / thumbnailHeight
 
-			cy.pan(cyPanNew);
+			cy.pan(cyPanNew)
 		}
 
 	}
@@ -214,18 +216,18 @@
 			var $this = $(this)
 				, data = $this.data('navigator')
 				, options = typeof option == 'object' && option
-				;
+				
 			if (!data) {
-				$this.data('navigator', (data = new Navigator(this, options)));
+				$this.data('navigator', (data = new Navigator(this, options)))
 			}
 			// TODO add handling of more function arguments
 			if (typeof option == 'string') {
-				data[option]();
+				data[option]()
 			}
 		})
 	}
 
-	$.fn.cytoscapeNavigator.Constructor = Navigator;
+	$.fn.cytoscapeNavigator.Constructor = Navigator
 
 	$.fn.cytoscapeNavigator.defaults = {
 		container: false
@@ -243,8 +245,8 @@
 			borderWidth: 0
 		}
 	, live: true // if true than cy is moved when dragging, otherwise it will be done when dragging was finished
-	};
+	}
 
-	$.fn.cyNavigator = $.fn.cytoscapeNavigator;
+	$.fn.cyNavigator = $.fn.cytoscapeNavigator
 
-})(jQuery);
+})(jQuery)
